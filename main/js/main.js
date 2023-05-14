8793162c4f08b266e787a7046e26d608b8c38459
Mohammad Taiham <mohammadtaiham.dryboss@gmail.com>
2023-05-14 23:57:35 +0600

    ui and js progress

--- main/js/main.js
+++ main/js/main.js
@@ -4,9 +4,14 @@ const drawCells = (gameBoard) => {
   for(let x=1; x<11; x++){
     for(let y=1; y<11; y++){
       const cell = document.createElement("div");
+      const cellImageCrossTurn = document.createElement("img");
+      cellImageCrossTurn.style.width = "100%";
+      cellImageCrossTurn.src = "images/icons/cross-turn.png";
+      cell.appendChild(cellImageCrossTurn);
       cell.style.gridColumnStart = x;
       cell.style.gridRowStart = y;
       cell.classList.add("cell");
+      cell.classList.add("blank");
       gameBoard.appendChild(cell);
     }
   }
